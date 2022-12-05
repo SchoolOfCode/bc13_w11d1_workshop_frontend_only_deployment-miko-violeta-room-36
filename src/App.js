7dcda83ad0bb7961c8b2b6ce541c4ddc888e441d
violetacf <violeta@cique.es>
2022-12-05 16:52:53 +0000

    inpuuut still not clearing

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState, useRef } from 'react';
+import { useState } from 'react';
 import './App.css';
 import Button from './Components/Button/Button';
 import Input from './Components/Input/Input';
@@ -7,24 +7,16 @@ import List from './Components/List/List';
 function App() {
   const [inputValue, setInputValue] = useState('');
   const [listState, setListState] = useState([]);
-  const input = useRef(null);
 
   function getInputValue(e) {
     setInputValue(e.target.value);
   }
+
   function addToList(e) {
-    let addedToList = inputValue;
     if (inputValue !== '') {
-      setListState([...listState, addedToList]);
+      setListState([...listState, inputValue]);
     }
     setInputValue('');
-  }
-
-  function handleSubmit(e) {
-    console.log('handleSubmit');
-    e.preventDefault(); // prevent page refresh
-    // clear all input values in the form:
-    input.current.value('');
   }
 
   return [
@@ -33,19 +25,10 @@ function App() {
         <h1>This is a list of things that everyone is thankful for</h1>
       </div>
       <div className="input-container">
-        <Input
-          onChange={getInputValue}
-          onSubmit={handleSubmit}
-          placeholder="I'm thankful for..."
-        />
+        <Input onChange={getInputValue} placeholder="I'm thankful for..." />
       </div>
       <div className="button-container">
-        <Button
-          type="submit"
-          onClick={addToList}
-          onSubmit={handleSubmit}
-          buttonText="Add to list"
-        />
+        <Button type="submit" onClick={addToList} buttonText="Add to list" />
       </div>
       <div className="list-container">
         {listState.map((list) => {
