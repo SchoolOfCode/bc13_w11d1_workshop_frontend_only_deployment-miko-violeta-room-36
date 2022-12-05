9cc89de5b831caf990595cc3ae256484f5894777
violetacf <violeta@cique.es>
2022-12-05 15:27:22 +0000

    background

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-
 import { useState } from 'react';
 import './App.css';
 import Button from './Components/Button/Button';
@@ -6,29 +5,27 @@ import Input from './Components/Input/Input';
 import List from './Components/List/List';
 
 function App() {
-  const [inputValue, setInputValue]=useState("")
-  const [listState, setListState]=useState([])
+  const [inputValue, setInputValue] = useState('');
+  const [listState, setListState] = useState([]);
 
-  function getInputValue(e){
-    setInputValue(e.target.value)
+  function getInputValue(e) {
+    setInputValue(e.target.value);
   }
-  function addToList(e){
-    setListState([...listState, inputValue])
+  function addToList(e) {
+    setListState([...listState, inputValue]);
   }
 
   return [
     <div className="app-container">
       <h1>This is a list of things that everyone is thankful for</h1>
-      <Input onChange={getInputValue}/>
-      <Button onClick={addToList} buttonText="Add to list"/>
-      {listState.map((list)=>{
-        return <List listText={list}/>
+      <Input onChange={getInputValue} />
+      <Button onClick={addToList} buttonText="Add to list" />
+      {listState.map((list) => {
+        return <List listText={list} />;
       })}
-    </div>,
-    <div className='footer-container'>
       <p>This app was created by Violeta Cique Fernández & Mikolaj Zagrodzki</p>
-      <img src='./LOGO.png'/>
-    </div>
+      <img src="./LOGO.png" />
+    </div>,
   ];
 }
 
